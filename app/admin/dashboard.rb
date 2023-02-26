a64870f9111f9c72ffb9f36a53af8c6c45e62d88
Matt Hopkins <moebaca@hotmail.com>
2023-02-26 15:19:39 +0900

    Updated dashboard to show points

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,18 +4,30 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc { I18n.t("active_admin.dashboard") } do
 
-    columns do
-      column do
-        panel "Current Points" do
-          Point.first do |point|
-            point do
-              selectable_column
-              column "My Custom Title", :total
+  panel "Points Information" do
+    tabs do
+      tab :current_points do
+        table do
+          thead do
+            tr do
+              th "Total Points"
+              th "Updated"
+            end
+          end
+          tbody do
+            tr do
+              th Point.first.total
+              th Point.first.created_at
             end
           end
         end
       end
+    
+      tab :points_history do
+      end
+
     end
+  end
 
   end # content
 end
